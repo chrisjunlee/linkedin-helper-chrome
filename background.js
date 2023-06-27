a86e4e2e83fdeeaa575d3a786db348fe24d3adde
chrisjunlee <chrisjunlee@gmail.com>
2023-06-26 22:47:24 -0700

    Added date pre-fix. Notification working!

--- background.js
+++ background.js
@@ -9,86 +9,86 @@ chrome.commands.onCommand.addListener(function(command) {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
-        func: extractPosting
+        func: App
       }); 
     });
   }
 });
 
-function extractPosting() {
-  var jobTitle = document.querySelector('h1.jobs-unified-top-card__job-title').textContent.trim()
-  var companyName = document.querySelector('span.jobs-unified-top-card__company-name').innerText.trim()
-  var url = window.location.href.split("?").shift();
+const App = () => {
+  function alertExtract(msg) {
+    var alertElement = document.createElement("div")
+    alertElement.style.fontSize = "1em"
+    alertElement.style.fontFamily = "Monaco";
+    alertElement.style.fontStyle = "normal";
+    alertElement.style.fontWeight = "normal";
+    alertElement.style.boxShadow = "0px 0px 16px 0px #CBCBCB";
+    alertElement.style.border = "1px solid #D9D900";
+    alertElement.style.zIndex = "100000001";
+    alertElement.style.textAlign = "center";
+    alertElement.style.color = "#444444";
+    alertElement.style.backgroundColor = "#BEE5B0";
+    alertElement.style.position = "fixed";
+    alertElement.style.borderRadius = ".25em";
+    alertElement.innerHTML = msg;
+    alertElement.style.boxSizing = "content-box";
+    alertElement.style.height = "2em";
+    alertElement.style.lineHeight = "2em";
+    alertElement.style.width = "fit-content";
+    alertElement.style.padding = "0px";
+    alertElement.style.margin = "0px";
+    alertElement.style.top = "50px";
+    alertElement.style.left = "50px";
 
-  var extractedText = [companyName, jobTitle, url].join('\t');
+    let notif = document.body.appendChild(alertElement);
 
-  console.log(companyName, jobTitle, url)
-
-  navigator.clipboard.writeText(extractedText)
-    .then(function() {
-      console.log("URL segment copied:", extractedText)
-
-      // alertExtract();
-    })
-    .catch(function(error) {
-      console.error("Failed to copy URL segment to clipboard:", error);
-    });
-}
-
-chrome.runtime.onMessage.addListener((msg) => {
-  console.log(msg.text);
-  });
-
-
-function alertExtract() {
-  var alertElement;
-
-  if (opts.alertOnExtract) {
-    alertElement =  document.createElement('div');
-    alertElement.style.fontSize = opts.alertOnExtractSize;
-    alertElement.style.fontFamily = 'Monaco';
-    alertElement.style.fontStyle = 'normal';
-    alertElement.style.fontWeight = 'normal';
-    alertElement.style.boxShadow = '0px 0px 16px 0px #CBCBCB';
-    alertElement.style.border = '1px solid #D9D900';
-    alertElement.style.zIndex = '100000001';
-    alertElement.style.textAlign = 'center';
-    alertElement.style.color = '#444444';
-    alertElement.style.backgroundColor = '#FFFF5C';
-    alertElement.style.position = 'fixed';
-    alertElement.style.borderRadius = '.25em';
-    alertElement.innerHTML = "Auto Copied";
-    alertElement.style.boxSizing = 'content-box';
-    alertElement.style.height = '2em';
-    alertElement.style.lineHeight = '2em';
-    alertElement.style.width = '7em';
-    alertElement.style.padding = '0px';
-    alertElement.style.margin = '0px';
-    alertElement.style.top = '50px';
-    alertElement.style.left = '50px';
+    var duration = 3 * 1000;
+    setTimeout(() => fade(notif, duration), 0)
   }
 
-  document.body.appendChild(alertElement);
-
-  var duration = parseFloat(opts.alertOnExtractDuration) * 1000;
-  console.log("Fade duration: " + duration);
-
-  sleep(duration).then(() => {
-    fade(el, 5);
-  });
-}
-
-function fade(el, speed) {
-  var timer;
-  if (el.style) {
-    el.style.opacity = '1';
-  }
-  timer = setInterval(function () {
-    el.style.opacity = parseFloat(el.style.opacity) - .02;
-    if (el.style.opacity <= 0) {
-      clearInterval(timer);
-      document.body.removeChild(el);
+  function fade(el, speed) {
+    var timer;
+    if (el.style) {
+      el.style.opacity = "1";
     }
-  },
-    speed);
+    timer = setInterval(function () {
+      el.style.opacity = parseFloat(el.style.opacity) - 0.02;
+      if (el.style.opacity <= 0) {
+        clearInterval(timer);
+        document.body.removeChild(el);
+      }
+    }, speed);
+  }
+
+  function extractPosting() {
+    let date = new Date()
+    const dateString = date.toLocaleDateString('en-us', {month:'numeric', day:'2-digit', year: "2-digit"})
+
+    var jobTitle = document.querySelector('h1.jobs-unified-top-card__job-title').textContent.trim()
+    var companyName = document.querySelector('span.jobs-unified-top-card__company-name').innerText.trim()
+    var url = window.location.href.split("?").shift();
+
+    var extractedText = [dateString, companyName, jobTitle, url].join('\t');
+
+    console.log(companyName, jobTitle, url)
+
+    navigator.clipboard.writeText(extractedText)
+      .then(function() {
+        console.log("URL segment copied:", extractedText)
+        console.log('extractedText', extractedText)
+
+        alertExtract(extractedText.replace(/\t/g, '&nbsp;'))
+      })
+      .catch(function(error) {
+        console.error("Failed to copy URL segment to clipboard:", error);
+      });
+  }
+
+  chrome.runtime.onMessage.addListener((msg) => {
+    console.log(msg.text);
+    });
+
+    extractPosting()
 }
+
+
